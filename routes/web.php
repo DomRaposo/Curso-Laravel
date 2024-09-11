f146bda1b82d9d62aea32a0d2c991e86e3014b63
Felipe Oliveira <felipe_ol@outlook.com>
2024-09-10 22:55:58 -0300

    iniciando estudo para Rota Carrinho

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\SiteController;
+use App\Http\Controllers\CarrinhoController;
 
 
 Route::resource('produtos', ProdutoController::class);
@@ -13,3 +14,4 @@ Route::get('/produto/{slug}',[SiteController::class, 'details'])->name('site.det
 
 Route::get('/categoria/{id}',[SiteController::class, 'categoria'])->name('site.categoria');
 
+Route::get('/carrinho',[CarrinhoController::class,'carrinhoLista'])->name('site.carrinho');
